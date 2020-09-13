1c71457187fde292c96d0b40401055af16afaac4
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-14 00:27:02 +0900

    pathパッケージとpath/filepathパッケージの使い分け

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"io"
 	"log"
-	"os/user"
+	"net/http"
+	"os"
+	"path"
 	"path/filepath"
 )
 
 func main() {
-	u, err := user.Current()
+	cwd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	// "\"でパス文字列を統合しない
-	dir := filepath.Join(u.HomeDir, ".config", "myapp")
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/", func(w htto.ResponseWriter, r *http.Request) {
+		//httpリクエストは論理パスなのでpathパッケージ
+		if ok, err := path.Match("/data/*.txt", r.URL.Path); err != nil || !ok {
+			http.NotFound(w, r)
+			return
+		}
+
+		//以降はっパスを物理パスとして扱うのでpath/filepathパッケージ
+		name := filepath.Join(cwd, "data", filepath.Base(r.URL.Path))
+		f, err := os.Open(name)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		defer f.close()
+		io.Copy(w, f)
+	})
+	http.ListenAndServe(":8080", nil)
 }
