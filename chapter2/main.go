1633cc76b4403bdc4c258c1bd9af960866541df4
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-14 10:38:19 +0900

    cp932からutf-8へのデコード処理

--- chapter2/main.go
+++ chapter2/main.go
@@ -1,23 +1,20 @@
 package main
 
-import "os"
-
-func doSomething() error {
-	err := os.MkdirAll("newdir", 0755)
-	if err != nil {
-		return err
-	}
-
-	defer os.RemoveAll("newdir")
-
-	f, err := os.Create("newdir/newfile")
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	return nil
-}
+import (
+	"log"
+	"os/exec"
+)
 
 func main() {
-	doSomething()
+	cmd := exec.Command("my-app")
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err = japanese.shiftJIS.NewDecoder().Bytes(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.print(string(b))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.15
+
+require golang.org/x/text v0.3.3 // indirect
